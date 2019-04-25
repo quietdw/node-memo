92d2c98012902546156601dd4da8f1ab5e329ee8
quietdw <1044569102@qq.com>
2019-04-25 21:23:18 +0800

    项目初始化 正在调试webpack测试

---
+++ app.js
@@ -0,0 +1,44 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views')); // 设置模板路径
+app.set('view engine', 'ejs'); // 设置模板名称
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({
+  extended: false
+}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public'))); // 设置静态目录
+
+// 路由
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
---
+++ src/mod/b.js
@@ -0,0 +1,6 @@
+var a = require('./a.js').a
+
+module.expots = {
+  b: 'bbb',
+  a: a
+}
