a48c2950d379e62a03986931384f8c0b04dd4101
quietdw <1044569102@qq.com>
2019-04-26 00:29:03 +0800

    修改配置文件

--- src/webpack.config.js
+++ src/webpack.config.js
@@ -2,7 +2,7 @@ var webpack = require('webpack')
 var path = require('path') // 保证任何情况下都能获取到正确的路径，不会因启动程序的路径不同而报错
 
 module.exports = {
-  entry: path.join(__dirname, "js/app/index.js"),
+  entry: path.join(__dirname, "./app/index.js"),
   output: {
     path: path.join(__dirname, "../public/js/"),
     filename: 'index.js'
