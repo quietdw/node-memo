6ac1c967a483dc7e9ca0dd63de8d30dc47d9649c
quietdw <1044569102@qq.com>
2019-04-27 16:04:20 +0800

    引用jquery

--- src/js/mod/toast.js
+++ src/js/mod/toast.js
@@ -1,3 +1,4 @@
+require('less/toast.less')
 let $ = require('jquery');
 
 class Toast {
@@ -11,18 +12,14 @@ class Toast {
 
 
   createToast() {
-    let template = `<div>${this.message}</div>`
+    let template = `<div class="toast">${this.message}</div>`
     this.$toast = $(template)
     $('body').append(this.$toast)
   }
 
   showToast() {
     this.$toast.fadeIn(300, () => {
-      setTimeout(() => {
-        this.$toast.fadeOut(300, () => {
-          this.$toast.remove()
-        })
-      }, this.duration);
+
     })
   }
 }
