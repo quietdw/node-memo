833421644dd26c45bf67d823ba7af316f2189270
quietdw <1044569102@qq.com>
2019-04-27 14:55:31 +0800

    toast.js toast 的 class，toast的样式

---
+++ src/js/mod/toast.js
@@ -0,0 +1,34 @@
+let $ = require('jquery');
+
+class Toast {
+  constructor(message, duration) {
+    this.message = message
+    this.duration = duration
+    this.createToast()
+    this.showToast()
+  }
+
+
+
+  createToast() {
+    let template = `<div>${this.message}</div>`
+    this.$toast = $(template)
+    $('body').append(this.$toast)
+  }
+
+  showToast() {
+    this.$toast.fadeIn(300, () => {
+      setTimeout(() => {
+        this.$toast.fadeOut(300, () => {
+          this.$toast.remove()
+        })
+      }, this.duration);
+    })
+  }
+}
+
+function toast(message, duration) {
+  return new Toast(message, duration)
+}
+
+module.exports.toast = toast
