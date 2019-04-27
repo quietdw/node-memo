04c6d1f1d7da9505ee79b9100619244859be2485
quietdw <1044569102@qq.com>
2019-04-27 23:52:23 +0800

    测试

--- src/js/app/index.js
+++ src/js/app/index.js
@@ -1,6 +1,10 @@
 var toast = require('../mod/toast.js').toast
+var eventBus = require('../mod/eventBus.js')
 
-console.log('----')
+console.log('------')
 console.log(toast)
+console.log(eventBus)
+
+window.eventBus = eventBus
 
 toast('hello')
