1e1cd486f8ac0cf77d054c7685e5c6ddd6bc63e0
quietdw <1044569102@qq.com>
2019-04-30 21:35:42 +0800

    测试首页引用less样式

--- src/js/app/index.js
+++ src/js/app/index.js
@@ -1,10 +1,8 @@
+require('less/index.less')
 var toast = require('../mod/toast.js').toast
 var eventBus = require('../mod/eventBus.js')
 
 console.log('------')
-console.log(toast)
-console.log(eventBus)
 
-window.eventBus = eventBus
 
 toast('hello')
